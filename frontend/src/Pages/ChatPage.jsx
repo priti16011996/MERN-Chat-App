f57529c34c40cb193fc8d128178b2ce61146b69f
Priti <priti16maurya@gmail.com>
2026-06-08 23:58:48 +0530

    Now Tern to design React Chat Page getting user info from local storage and based on Chat Page

--- frontend/src/Pages/ChatPage.jsx
+++ frontend/src/Pages/ChatPage.jsx
@@ -1,30 +1,15 @@
-import React, { useEffect } from 'react'
-import axios from "axios"
-import { useState } from 'react';
+import { ChatState } from "../context/ChatProvider"
+
 
 function ChatPage() {
-  let [chats, setChats] = useState([]);
-  const fetchChats = async () => {
-    try {
-      const { data } = await axios.get("/api/v1/chat");
-      setChats(data)
-
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  useEffect(() => {
-    fetchChats();
-  }, []);
-
+  const { user } = ChatState();
   return (
-    <div>
-      <ul>
-      {chats.map((chat)=>(
-        <li key={chat._id}>{chat.chatName}</li>
-      ))}
-      </ul>
+    <div style={{ width: "100%" }}>
+      {/* {user && <SideDrawer />} */}
+      <Box>
+        {/* {user && <MyChats />} */}
+        {/* {user && <ChatBox />} */}
+      </Box>
     </div>
   )
 }
